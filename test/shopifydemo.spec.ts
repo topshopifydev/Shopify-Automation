17d70e8af26e327f32159c13ae533d170ddcf95d
topshopifydev <lycanluciferdev@gmail.com>
2025-04-12 15:35:13 +0530

    optimize testcases

--- test/shopifydemo.spec.ts
+++ test/shopifydemo.spec.ts
@@ -1,44 +1,39 @@
 import { test, expect } from "@playwright/test";
 
-//test.describe.configure({ timeout: 40000 }); // 60 seconds
+test.describe.configure({ timeout: 40000 }); // 60 seconds
 
 test.describe("Meetanshi Shopify Apps", () => {
+  test("Meetanshi PDF Catalog", async ({ page, context }) => {
+    await page.goto("https://pdfdemo.myshopify.com/");
+    await page.locator("#password").fill(process.env.PASSWORD ?? "");
+    await page.getByRole("button", { name: "Enter" }).click();
+    await page.locator("#HeaderMenu-collection").click();
+    await page.getByRole("link", { name: "Clothing" }).click();
 
-  test("check url", async ({ page }) => {
-    await page.goto("https://www.playwright.dev/");
-   });
+    await expect(page.getByRole("button", { name: "Print Pdf" })).toBeVisible();
 
-  // test("Meetanshi PDF Catalog", async ({ page, context }) => {
-  //   await page.goto("https://pdfdemo.myshopify.com/");
-  //   await page.locator("#password").fill(process.env.PASSWORD ?? "");
-  //   await page.getByRole("button", { name: "Enter" }).click();
-  //   await page.locator("#HeaderMenu-collection").click();
-  //   await page.getByRole("link", { name: "Clothing" }).click();
+    const [newPage] = await Promise.all([
+      context.waitForEvent("page"),
+      page.getByRole("button", { name: "Print Pdf" }).click(),
+    ]);
 
-  //   await expect(page.getByRole("button", { name: "Print Pdf" })).toBeVisible();
+    await newPage.waitForLoadState("domcontentloaded");
+    const title = await newPage.title();
+    expect(title).toBe("Pdf Preview");
+    console.log("PDF preview page opened successfully");
+  });
 
-  //   const [newPage] = await Promise.all([
-  //     context.waitForEvent("page"),
-  //     page.getByRole("button", { name: "Print Pdf" }).click(),
-  //   ]);
-
-  //   await newPage.waitForLoadState("domcontentloaded");
-  //   const title = await newPage.title();
-  //   expect(title).toBe("Pdf Preview");
-  //   console.log("PDF preview page opened successfully");
-  // });
-
-  // test("Meetanshi Whatsapp share", async ({ page }) => {
-  //   await page.goto("https://whatsapp-share-button.myshopify.com/");
-  //   await page.locator("#password").fill(process.env.PASSWORD ?? "");
-  //   await page.getByRole("button", { name: "Enter" }).click();
-  //   await page.locator("#HeaderMenu-catalog").click();
-  //   await page.getByRole("link", { name: "Freak 5 EP" }).click();
-  //   const whatsappLink = page.locator('a[data-action="share/whatsapp/share"]');
-  //   await expect(whatsappLink).toBeVisible();
-  //   console.log("Whatsapp share button is visible");
-  // });
-/*  test("Meetanshi shipping per item", async ({ page }) => {
+  test("Meetanshi Whatsapp share", async ({ page }) => {
+    await page.goto("https://whatsapp-share-button.myshopify.com/");
+    await page.locator("#password").fill(process.env.PASSWORD ?? "");
+    await page.getByRole("button", { name: "Enter" }).click();
+    await page.locator("#HeaderMenu-catalog").click();
+    await page.getByRole("link", { name: "Freak 5 EP" }).click();
+    const whatsappLink = page.locator('a[data-action="share/whatsapp/share"]');
+    await expect(whatsappLink).toBeVisible();
+    console.log("Whatsapp share button is visible");
+  });
+  test("Meetanshi shipping per item", async ({ page }) => {
     await page.goto("https://shipping-per-item.myshopify.com/");
     await page.locator("#password").fill(process.env.PASSWORD ?? "");
     await page.getByRole("button", { name: "Enter" }).click();
@@ -104,5 +99,5 @@ test.describe("Meetanshi Shopify Apps", () => {
       page.locator("//p[contains(text(), 'Rate Name')]")
     ).toBeVisible();
     console.log("Shipping zipcode Rate visible successfully");
-  });*/
+  });
 });
